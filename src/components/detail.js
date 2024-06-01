c9a73bb7d8e5c524be534f956cb12ac315a686e3
NguyenQuanBao <gabynexo221@gmail.com>
2024-06-02 00:18:36 +0700

    giao dien trang chu

--- src/components/detail.js
+++ src/components/detail.js
@@ -1,7 +1,34 @@
 import "../css/detail.css"
 import Footer from "./footer";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 import Navbar from "./navbar";
 function Detail() {
+    const [house, setHouse] = useState({});
+
+    const a = Number(house.price);
+    const formattedNumber = a.toLocaleString();
+    console.log(house.price)
+    const params = useParams();
+
+    function formatCurrency(amount) {
+        return amount.toLocaleString('vi-VN', { style: 'currency', currency: 'VND' });
+    }
+
+    async function getHouse() {
+        const res = await axios.get(`http://localhost:8080/api/house/${params.id}`)
+
+        setHouse(res.data);
+    }
+
+    useEffect(() => {
+        getHouse()
+    }, []);
+    const handleViewDirections = () => {
+        const googleMapsUrl = `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(house.address)}`;
+        window.open(googleMapsUrl, '_blank');
+    };
     return (
         <div>
             <header>
@@ -9,26 +36,89 @@ function Detail() {
             </header>
 
             <div class="blog-single">
-                <div class="container" style={{ alignItems: 'center' }}>
-                    <div class="align-items-center">
-                        <div class=" m-15px-tb">
-                            <article class="article"  style={{ borderBottom: "100px", bottom:'20%' }}>
-                                <div class="article-img text-center">
-                                    <img class="img-fluid" src="https://tienganhikun.com/upload/images/house_ikun.jpg" title="" alt="" />
+                <div class="container-fluid " >
+
+                    <div class="row justify-content-center">
+
+                        <div class="col-lg-8 m-15px-tb">
+                            <h1>Tên nhà</h1>
+                            <article class="article">
+                                <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                                    <div class="carousel-inner">
+                                        {house.images?.map((item, index) => (
+                                            <div className={`carousel-item ${index === 0 ? 'active' : ''}`}>
+                                                <div>
+                                                    <img src={process.env.PUBLIC_URL + '/img/' + (item.nameImage)}
+                                                        class="d-block w-100" alt={`Carousel Image ${index + 1}`} />
+                                                </div>
+
+                                            </div>
+                                        ))}
+                                    </div>
+
+                                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                        <span class="visually-hidden">Previous</span>
+                                    </button>
+                                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                        <span class="visually-hidden">Next</span>
+                                    </button>
                                 </div>
-                                <div class="article-title">
-                                    <h2>Tên ngôi nhà</h2>
-                                </div>
-                                <h2>Giá(VND)</h2>
-                                <div class="article-content">
-                                    Hiển thị thông tin chi tiết ngôi nhà ở đây
+                                <div className="test" style={{ display: "inline-flex" }}>
+                                    <div className="test1">
+                                        <div className="article-title" style={{ display: 'inline-flex' }}>
+                                            {/* <h2>{house.address}</h2> */}
+                                            <h2>Tên nhà</h2>
+                                            <small style={{ paddingTop: '8%' }}>({formattedNumber} VND/ ngày)</small>
+                                        </div>
+                                        <div style={{display:'flex' , paddingBottom:"15px"}}>
+                                            <h6>Số phòng ngủ:3 {house.numberOfBedRoom}, </h6>
+                                            <h6>Số phòng tắm:4 {house.numberOfBathRoom}</h6>
+                                        </div>
+
+                                        <div className="article-content">
+                                            <div style={{ display: 'flex', alignItems: 'center' }}>
+                                                <h5 style={{ marginRight: '8px' }}>Địa chỉ: {house.name}</h5>
+                                                <a style={{ marginBottom: '13px', marginLeft: '13px' }}
+                                                    onClick={handleViewDirections}>Xem chỉ dẫn</a>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                    <div className="test2">
+                                        <form>
+                                            <div class="container">
+                                                <div class="row justify-content-end" style={{ paddingTop: '5%' }}>
+                                                    <div class="card shadow-sm w-100" style={{ left: "140%", borderRadius:"10%" }}>
+                                                        <div class="card-body">
+                                                            <h4 class="mb-4">Đặt thuê nhà</h4>
+                                                            <div class="input-group w-100" style={{paddingBottom:'5%'}}>
+                                                                <input type="date" aria-label="First date" placeholder="Ngày đặt" class="form-control w-50"     />
+                                                                <input type="date" aria-label="Last date" class="form-control w-50" />
+                                                            </div>
+                                                            <div class="form-group" style={{display:'flex'}}>
+                                                                <label><b>Số ngày thuê:</b></label>
+                                                                <p class="font-weight-bold" id="rentalDays">0 ngày</p>
+                                                            </div>
+                                                            <div style={{display:'flex'}}>
+                                                                <label><b>Tổng tiền:</b></label>
+                                                                <p class="font-weight-bold" id="rentalDays">1,200,00</p>
+                                                            </div>
+                                                            <div class="text-center">
+                                                                <button type="button" class="btn btn-primary">Thuê nhà</button>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
                                 </div>
                             </article>
-
-                            <div class="contact-form article-comment" >
-                                <div>
-                                    <h4 >Mô tả</h4>
-                                </div>
+                            <div class="contact-form article-comment">
+                                <h4>Nhận xét</h4>
                                 <form id="contact-form" method="POST">
                                     <div class="row">
 
@@ -48,9 +138,9 @@ function Detail() {
                     </div>
                 </div>
             </div>
-            <div>
+            <footer>
                 <Footer />
-            </div>
+            </footer>
         </div>
     )
 }
--- src/components/home.js
+++ src/components/home.js
@@ -6,155 +6,153 @@ import Footer from "./footer";
 import Navbar from "./navbar";
 
 export default function Home() {
-    const [houses, setHouses] = useState([]);
-    const [search, setSearch] = useState('');
-    const userName = sessionStorage.getItem('userName');
-    const password = sessionStorage.getItem('password');
-    const roles = sessionStorage.getItem('roles');
-    const idAccount = sessionStorage.getItem('account_id');
-    const checkRole = useState('')
-    const [currentPage, setCurrentPage] = useState(1);
-    const [itemsPage, setItemsPage] = useState(5);
-    const totalPages = Math.ceil(houses.length / itemsPage);
-    const [price, setPrice]
-        = useState(0);
+    // const [houses, setHouses] = useState([]);
+    // const [search, setSearch] = useState('');
+    // const userName = sessionStorage.getItem('userName');
+    // const password = sessionStorage.getItem('password');
+    // const roles = sessionStorage.getItem('roles');
+    // const idAccount = sessionStorage.getItem('account_id');
+    // const checkRole = useState('')
+    // const [currentPage, setCurrentPage] = useState(1);
+    // const [itemsPage, setItemsPage] = useState(5);
+    // const totalPages = Math.ceil(houses.length / itemsPage);
+    // const [price, setPrice]
+    //     = useState(0);
 
-    function formatCurrentcy(amount) {
-        return amount.toLocaleString('vi-VN', { style: 'currency', currency: 'VND' })
-    }
-    const getCurrentPageData = () => {
-        const startIndex = (currentPage - 1) * itemsPage;
-        const endIndex = startIndex + itemsPage;
-        return houses.slice(startIndex, endIndex);
-    };
-    const currentPageData = getCurrentPageData();
+    // function formatCurrentcy(amount) {
+    //     return amount.toLocaleString('vi-VN', { style: 'currency', currency: 'VND' })
+    // }
+    // const getCurrentPageData = () => {
+    //     const startIndex = (currentPage - 1) * itemsPage;
+    //     const endIndex = startIndex + itemsPage;
+    //     return houses.slice(startIndex, endIndex);
+    // };
+    // const currentPageData = getCurrentPageData();
 
-    const renderPageItems = () => {
-        const pageItems = [];
-        for (let i = 1; i <= totalPages; i++) {
-            const isActive = i === currentPage ? 'active' : '';
+    // const renderPageItems = () => {
+    //     const pageItems = [];
+    //     for (let i = 1; i <= totalPages; i++) {
+    //         const isActive = i === currentPage ? 'active' : '';
 
-            pageItems.push(
-                <li className={`page-item ${isActive}`} key={i}>
-                    <a className="page-link" onClick={() => currentPage + 1}>{i}</a>
-                </li>
-            );
-        }
-        return pageItems;
-    };
-    async function getList() {
-        const response = await axios.get(`http://localhost:8080/api/house?name=${search}`);
-        // console.log(response.data)
-        setHouses(response.data);
+    //         pageItems.push(
+    //             <li className={`page-item ${isActive}`} key={i}>
+    //                 <a className="page-link" onClick={() => currentPage + 1}>{i}</a>
+    //             </li>
+    //         );
+    //     }
+    //     return pageItems;
+    // };
+    // async function getList() {
+    //     const response = await axios.get(`http://localhost:8080/api/house?name=${search}`);
+    //     // console.log(response.data)
+    //     setHouses(response.data);
 
+    //     if (roles === 'all') {
+    //         checkRole = null;
+    //     }
 
-
-        if (roles === 'all') {
-            checkRole = null;
-        }
-
-    }
-    useEffect(() => {
-        getList()
-    }, [search])
+    // }
+    // useEffect(() => {
+    //     getList()
+    // }, [search])
     return (
-        
-        <div>
-            <header>
-                <Navbar />
-            </header>
-            <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel" >
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/b6e874107714719.5fad336f21e5b.png" class="d-block w-100" alt="..." />
-                    </div>
-                    <div class="carousel-item">
-                        <img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/945451107714719.5fad336f20b9c.png" class="d-block w-100" alt="..." />
-                    </div>
-                    <div class="carousel-item">
-                        <img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/873875107714719.5fad336f1fe85.png" class="d-block w-100" alt="..." />
-                    </div>
-                </div>
-            </div>
+        <></>
+        // <div>
+        //     <header>
+        //         <Navbar />
+        //     </header>
+        //     <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel" >
+        //         <div class="carousel-inner">
+        //             <div class="carousel-item active">
+        //                 <img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/b6e874107714719.5fad336f21e5b.png" class="d-block w-100" alt="..." />
+        //             </div>
+        //             <div class="carousel-item">
+        //                 <img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/945451107714719.5fad336f20b9c.png" class="d-block w-100" alt="..." />
+        //             </div>
+        //             <div class="carousel-item">
+        //                 <img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/873875107714719.5fad336f1fe85.png" class="d-block w-100" alt="..." />
+        //             </div>
+        //         </div>
+        //     </div>
 
-            <div className="search" style={{ marginBottom: "5em", marginTop: "2em" }}>
-                <div className="container" >
-                    <form class="form-inline d-flex" >
+        //     <div className="search" style={{ marginBottom: "5em", marginTop: "2em" }}>
+        //         <div className="container" >
+        //             <form class="form-inline d-flex" >
 
-                        <input class="form-control my-sm-0" style={{ width: "400px", borderRadius: '20px', marginRight: "1%", borderColor: 'black' }} type="search" placeholder="Tìm nhà cho thuê" aria-label="Search" />
-                        <select class="form-select" aria-label="Default select example" style={{ width: "160px", marginRight: '0.5%', borderColor: 'black' }}>
-                            <option selected>Số phòng ngủ</option>
-                            <option value="1">One</option>
-                            <option value="2">Two</option>
-                            <option value="3">Three</option>
-                        </select>
-                        <select class="form-select" aria-label="Default select example" style={{ width: "160px", borderColor: 'black' }}>
-                            <option selected>Số phòng tắm</option>
-                            <option value="1">One</option>
-                            <option value="2">Two</option>
-                            <option value="3">Three</option>
-                        </select>
-                        <select class="form-select" aria-label="Default select example" style={{ width: "160px", marginLeft: '0.5%', borderColor: 'black' }}>
-                            <option selected>Giá</option>
-                            <option value="1">1 - 3 triệu</option>
-                            <option value="2">2 - 5 triệu</option>
-                            <option value="3">5 - 7 triệu</option>
-                            <option value="4">7 - 9 triệu</option>
-                        </select>
-                        <select class="form-select" aria-label="Default select example" style={{ width: "160px", marginLeft: '0.5%', borderColor: 'black' }}>
-                            <option selected>Trạng thái</option>
-                            <option value="1">Còn trống</option>
-                            <option value="2">Đã cho thuê</option>
-                            <option value="3">Đang nâng cấp</option>
-                        </select>
-                        <div style={{ marginLeft: '4%' }} >
-                            <button class="btn btn-danger  my-2 my-sm-0" type="submit" style={{ left: "20%" }}>Tìm kiếm</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-            <body>
+        //                 <input class="form-control my-sm-0" style={{ width: "400px", borderRadius: '20px', marginRight: "1%", borderColor: 'black' }} type="search" placeholder="Tìm nhà cho thuê" aria-label="Search" />
+        //                 <select class="form-select" aria-label="Default select example" style={{ width: "160px", marginRight: '0.5%', borderColor: 'black' }}>
+        //                     <option selected>Số phòng ngủ</option>
+        //                     <option value="1">One</option>
+        //                     <option value="2">Two</option>
+        //                     <option value="3">Three</option>
+        //                 </select>
+        //                 <select class="form-select" aria-label="Default select example" style={{ width: "160px", borderColor: 'black' }}>
+        //                     <option selected>Số phòng tắm</option>
+        //                     <option value="1">One</option>
+        //                     <option value="2">Two</option>
+        //                     <option value="3">Three</option>
+        //                 </select>
+        //                 <select class="form-select" aria-label="Default select example" style={{ width: "160px", marginLeft: '0.5%', borderColor: 'black' }}>
+        //                     <option selected>Giá</option>
+        //                     <option value="1">1 - 3 triệu</option>
+        //                     <option value="2">2 - 5 triệu</option>
+        //                     <option value="3">5 - 7 triệu</option>
+        //                     <option value="4">7 - 9 triệu</option>
+        //                 </select>
+        //                 <select class="form-select" aria-label="Default select example" style={{ width: "160px", marginLeft: '0.5%', borderColor: 'black' }}>
+        //                     <option selected>Trạng thái</option>
+        //                     <option value="1">Còn trống</option>
+        //                     <option value="2">Đã cho thuê</option>
+        //                     <option value="3">Đang nâng cấp</option>
+        //                 </select>
+        //                 <div style={{ marginLeft: '4%' }} >
+        //                     <button class="btn btn-danger  my-2 my-sm-0" type="submit" style={{ left: "20%" }}>Tìm kiếm</button>
+        //                 </div>
+        //             </form>
+        //         </div>
+        //     </div>
+        //     <body>
 
-                <div className="container"  >
-                    <h2 style={{ textAlign: 'center' }}>Danh sách các nhà đang cho thuê</h2>
-                    {currentPageData.map(houses => <div class="portfolioContainer " style={{ display: 'flex', flexWrap: "wrap" }}>
-                        <div class="col-sm-6 col-lg-3 col-md-4 webdesign illustrator">
-                            <Link to={`/detail/${house.id}`}>
-                                <div class="gal-detail thumb h-90">
-                                    <div class="image-popup" title="Screenshot-1">
-                                        <img src={process.env.PUBLIC_URL + '/img/' + (house.images[0]?.nameImage || '')} class="thumb-img" alt="work-thumbnail" />
-                                    </div>
-                                    <h5 class="text">{house.name}</h5>
+        //         <div className="container"  >
+        //             <h2 style={{ textAlign: 'center' }}>Danh sách các nhà đang cho thuê</h2>
+        //             {currentPageData.map(houses => <div class="portfolioContainer " style={{ display: 'flex', flexWrap: "wrap" }}>
+        //                 <div class="col-sm-6 col-lg-3 col-md-4 webdesign illustrator">
+        //                     <Link to={`/detail/${house.id}`}>
+        //                         <div class="gal-detail thumb h-90">
+        //                             <div class="image-popup" title="Screenshot-1">
+        //                                 <img src={process.env.PUBLIC_URL + '/img/' + (house.images[0]?.nameImage || '')} class="thumb-img" alt="work-thumbnail" />
+        //                             </div>
+        //                             <h5 class="text">{house.name}</h5>
 
-                                    <p class="text-muted"><small>{formatCurrentcy(house.price)}</small></p>
-                                    <p class="text-muted"><small>{house.address}</small></p>
-                                    <button type="button" class="btn btn-outline-primary" style={{ marginLeft: "50%" }} disabled>Trạng thái</button>
-                                </div>
-                            </Link>
-                        </div>
+        //                             <p class="text-muted"><small>{formatCurrentcy(house.price)}</small></p>
+        //                             <p class="text-muted"><small>{house.address}</small></p>
+        //                             <button type="button" class="btn btn-outline-primary" style={{ marginLeft: "50%" }} disabled>Trạng thái</button>
+        //                         </div>
+        //                     </Link>
+        //                 </div>
 
                         
-                    </div>
-                    )}
-                </div>
-                <div>
-                <nav aria-label="Page navigation example">
-                    <ul className="pagination justify-content-center">
-                        <li className={`page-item ${currentPage === 1 ? 'disabled' : ''}`}>
-                            <a className="page-link" onClick={() => setCurrentPage(currentPage - 1)}>Previous</a>
-                        </li>
-                        {renderPageItems()}
-                        <li className={`page-item ${currentPage === totalPages ? 'disabled' : ''}`}>
-                            <a className="page-link" onClick={() => setCurrentPage(currentPage + 1)}>Next</a>
-                        </li>
+        //             </div>
+        //             )}
+        //         </div>
+        //         <div>
+        //         <nav aria-label="Page navigation example">
+        //             <ul className="pagination justify-content-center">
+        //                 <li className={`page-item ${currentPage === 1 ? 'disabled' : ''}`}>
+        //                     <a className="page-link" onClick={() => setCurrentPage(currentPage - 1)}>Previous</a>
+        //                 </li>
+        //                 {renderPageItems()}
+        //                 <li className={`page-item ${currentPage === totalPages ? 'disabled' : ''}`}>
+        //                     <a className="page-link" onClick={() => setCurrentPage(currentPage + 1)}>Next</a>
+        //                 </li>
 
-                    </ul>
-                </nav>
-            </div>
+        //             </ul>
+        //         </nav>
+        //     </div>
 
-            </body>
-            <Footer />
-        </div>
+        //     </body>
+        //     <Footer />
+        // </div>
 
 
     )
